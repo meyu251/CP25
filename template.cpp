93eb63feb95e1052eb832366be9da8ec040059e1
Meir Yust <meyu251@gmail.com>
2024-11-30 23:34:29 +0200

    save

--- template.cpp
+++ template.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -7,7 +8,13 @@ using namespace std;
 
 int main(){
 
-
+    int n, sum;
+    cin >> n >> sum;
+    cin.ignore();
+    vector<int> numbers(n);
+    for(int i = 0; i < n; i++){
+        cin >> numbers[i];
+    }
 
     return 0;
 }
